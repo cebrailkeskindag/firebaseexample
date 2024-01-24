62daf35ca9504d55f2815733a47a6f0a6c8ee1f6
cebrailkeskindag <cebrailkeskindag1549@gmail.com>
2024-01-24 17:53:13 +0300

    delete files

--- android/app/src/main/kotlin/com/example/firebaseexample/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.firebaseexample
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/models/message_model.dart
+++
@@ -1,7 +0,0 @@
-class Message {
-  final String userID;
-  final String message;
-  final String time;
-
-  Message(this.userID, this.message, this.time);
-}
